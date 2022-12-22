17678c89a08783c211ed6a8d78fb9ddd77dd5407
Alex <>
2022-12-22 02:30:08 -0500

    add redboard code

---
+++ Redboard/blinkycount/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+const bool DEBUG = true;
+const byte BUTTON = 8;
+const byte LED = 11;
+
+const size_t MAX_ADDR = 2 << 15;
+
+const int COUNTDOWN_TIME = 30;
---
+++ Redboard/blinkycount/periph.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "config.h"
+
+void ledSet(bool on) {
+  digitalWrite(LED, on);
+}
+void ledAnalog(uint8_t i) {
+  analogWrite(LED, i);
+}
+void ledPulse(uint8_t d) {
+  static bool brightness_inc = true;
+  static uint8_t brightness = 0;
+
+  if (brightness_inc) {
+    brightness++;
+    if (brightness == 255) {
+      brightness_inc = false;
+    }
+  } else {
+    brightness--;
+    if (brightness == 0) {
+      brightness_inc = true;
+    }
+  }
+  ledAnalog(brightness);
+  delay(d);
+}
+
+bool getButton() {
+  return !digitalRead(BUTTON);
+}
+bool buttonPressed() {
+  return getButton();
+}
+bool buttonReleased() {
+  return !getButton();
+};
+bool waitForButtonRelease() {
+  while (buttonPressed())
+    ledPulse(8);
+}
